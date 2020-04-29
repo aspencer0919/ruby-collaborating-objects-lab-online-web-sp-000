a8a390549814c0b5aab53b4ed0e92097c225007e
Adam Spencer <aspencer0919@gmail.com>
2020-04-29 19:14:00 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -22,7 +22,11 @@ class Artist
   end
 
   def self.find_or_create_by_name(name)
-    self.all.detect {|artist| artist.name == name || Artist.new(name).save}
+    if self.find(name)
+      self.find(name)
+    else
+      self.create(name)
+    end
   end
 
   def print_songs
