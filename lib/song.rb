5f1a2e16a8a3ec9a7c3390899fbaff6ff177b8e1
Adam Spencer <aspencer0919@gmail.com>
2020-04-29 18:50:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,8 @@
 class Song
   attr_accessor :name, :artist
 
+  @@all = []
+
   def initialize(name)
     @name = name
   end
@@ -11,9 +13,10 @@ class Song
     new_song = self.new(song)
     new_song.artist_name = artist
     new_song
+    @@all << new_song
   end
 
-  def artist_name=(name)
+  def artist_name(name)
     self.artist = Artist.find_or_create_by_name(name)
     artist.add_song(self)
   end
