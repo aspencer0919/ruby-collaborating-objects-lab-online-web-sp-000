22b0f7126b55dd7585fb81a9ad1ee568141c47e6
Adam Spencer <aspencer0919@gmail.com>
2020-04-29 18:50:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,6 +16,10 @@ class Song
     @@all << new_song
   end
 
+  def self.all
+    @@all
+  end
+
   def artist_name(name)
     self.artist = Artist.find_or_create_by_name(name)
     artist.add_song(self)
