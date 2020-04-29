1da931c65a4ea3707c3422a7a84efe8fa4bc72df
Adam Spencer <aspencer0919@gmail.com>
2020-04-29 19:11:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,12 +16,12 @@ class Song
     artist_name, song_name, extra = filename.split(" - ")
     song = self.new(song_name)
     artist = Artist.find_or_create_by_name(artist_name)
-    artist.add_song(song)
+    Artist.add_song(song)
   end
 
   def artist_name
     self.artist = Artist.find_or_create_by_name(name)
-    artist.add_song(self)
+    Artist.add_song(self)
   end
 
 end
