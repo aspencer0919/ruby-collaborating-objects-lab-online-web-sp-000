d84f960163ee54ce3decf633734d20555f0893d5
Adam Spencer <aspencer0919@gmail.com>
2020-04-29 18:23:08 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,18 @@
 class Song
-  attr_accessor :title, :name
+  attr_accessor :title, :name, :artist
 
   @@song = []
 
   def initialize(name)
     @name = name
     @@song << self
+  end
+
+  def artist(artist)
+
+  end
+
+  def self.all
+    @@song
   end
 end
