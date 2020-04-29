519be81a5dc1930bc8cb0d016b9bee6c763a1b3e
Adam Spencer <aspencer0919@gmail.com>
2020-04-29 18:48:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,21 +1,21 @@
 class Song
-  attr_accessor :title, :name, :artist
+  attr_accessor :name, :artist
 
-  @@song = []
-
-  def initialize(name, artist = nil)
+  def initialize(name)
     @name = name
-    @artist = artist
-    @@song << self
   end
 
-  def artist
-    if @artist
-      @artist.name
-    end
+  def self.new_by_filename(file_name)
+    song = file_name.split(" - ")[1]
+    artist = file_name.split(" - ")[0]
+    new_song = self.new(song)
+    new_song.artist_name = artist
+    new_song
   end
 
-  def self.all
-    @@song
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
   end
+
 end
